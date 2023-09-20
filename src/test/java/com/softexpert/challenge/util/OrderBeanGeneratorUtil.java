eef70e7c1a0d4f1048392da2aead78e0a0e5d164
Igor Bruno <ibruno@close-upinternational.com.br>
2023-09-20 00:40:17 -0300

    I adapted it for Java 8

--- src/test/java/com/softexpert/challenge/util/OrderBeanGeneratorUtil.java
+++ src/test/java/com/softexpert/challenge/util/OrderBeanGeneratorUtil.java
@@ -3,14 +3,17 @@ package com.softexpert.challenge.util;
 import com.softexpert.challenge.bean.OrderBean;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class OrderBeanGeneratorUtil {
 
     public static OrderBean getOrderBean() {
 
         return OrderBean.builder().
-                amounts(List.of(10.0)).
+                amounts(Stream.of(10.0).collect(Collectors.toList())).
                 deliveryAmount(8.0).
                 discountAmount(5.0).
                 totalOrder(2).
